53dd5bb50aa5f22c5eb96e318cd4ad597ed2cb25
Rodrigo Junior <rodrjuniorsantos@gmail.com>
2024-07-06 09:36:31 -0300

    refactor: update readme

--- tests/test_module.py
+++ tests/test_module.py
@@ -1,10 +1,12 @@
 import unittest
 from chromotools.module import add
 
+
 class TestModule(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(1, 2), 3)
         self.assertEqual(add(-1, 1), 0)
+
 
 if __name__ == "__main__":
     unittest.main()
