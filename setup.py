91a34ae4277065ea4f46988309871232b5aea75a
Rodrigo Junior <rodrjuniorsantos@gmail.com>
2024-07-06 23:12:35 -0300

    fix: display video

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="chromotools",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[],
     author="Rodrigo J. R Santos",
